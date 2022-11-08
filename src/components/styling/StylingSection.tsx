f758b54abf026b2ae4b7b910d5252838645a6675
Riccardo Silvi <r.silvi@young.business>
2022-11-08 18:54:29 +0100

    mediaQueries section and media util

--- src/components/styling/StylingSection.tsx
+++ src/components/styling/StylingSection.tsx
@@ -1,11 +1,13 @@
 import { CssVars } from "./cssVars";
 import { Basics } from "./basics";
+import { MediaQueries } from "./mediaQueries";
 
 export const StylingSection = () => {
   return (
     <div>
       <Basics />
       <CssVars />
+      <MediaQueries />
     </div>
   );
 };
